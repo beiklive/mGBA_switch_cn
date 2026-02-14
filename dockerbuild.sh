cb6007e862d18d18ebf09eaf716251bc36f226af
重装小杰 <73771431+beiklive@users.noreply.github.com>
2026-02-14 17:05:44 +0800

    feat(dockerbuild): 添加cmake构建命令
    在dockerbuild.sh脚本中添加了具体的docker镜像版本，并增加了cmake构建命令以支持更详细的构建过程。
    
    docker run 命令中指定了使用 devkitpro/devkita64:20260202 镜像版本，确保构建环境的一致性和稳定性。
    
    增加了 cmake 命令，使用 DCMAKE_TOOLCHAIN_FILE 指定了工具链文件路径为 ../src/platform/switch/CMakeToolchain.txt，并以相对路径 .. 作为源码目录进行构建。

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,3 +1,7 @@
 # 进入docker环境，并将当前目录映射到docker环境的/mGBA目录下
 
-docker run -it --rm -v $PWD:/mGBA devkitpro/devkita64 /bin/bash
+docker run -it --rm -v $PWD:/mGBA devkitpro/devkita64:20260202 /bin/bash
+
+
+# cmake 命令
+cmake -DCMAKE_TOOLCHAIN_FILE=../src/platform/switch/CMakeToolchain.txt ..
